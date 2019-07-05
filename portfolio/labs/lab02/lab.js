8e00983a0e2834a97164eae0ab283d7bc1c1b8bd
siangyou-900103 <52515401+siangyou-900103@users.noreply.github.com>
2019-07-05 23:27:36 +0800

    Add files via upload

--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -3,9 +3,28 @@
 */
 
 
-function start(e) {
-
+function update() {
+var x =document.getElementById('in1').value;
+var ele =document.getElementById('info1');
+var y = ' ';
+for(var i=0; i<x.length; i++){
+ y += x.charAt(i)
+ y +='U+';
+ y += x.charCodeAt(i).toString(16).toUpperCase()
+ y +='<br>';
 }
+ele.innerHTML=y;
+document.getElementById('info3').innerHTML=
+document.getElementById('in1').value.replace('大數據','big data');
+}
+
+function start(e){
+    document.getElementById('in1').addEventListener("input",update);
+    update();
+}
+
+
+
 
 /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
