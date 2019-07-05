f36a306d7376bba6a57d76a218efb94add8b230f
siangyou-900103 <52515401+siangyou-900103@users.noreply.github.com>
2019-07-05 23:14:24 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,39 @@
 
 
 function start(e) {
+  var name =[
+  {
+    char:'葉',
+    big5:'B8AD',
+    cns:'1-6634',
+    unicode:'8449'
+  },
+  {
+    char:'相',
+    big5:'ACDB',
+    cns:'1-525E',
+    unicode:'76F8'
+  },
+  {
+    char:'佑',
+    big5:'A6F6',
+    cns:'1-4877',
+    unicode:'4F51'
+  }
+  ];
 
+  var ele = document.getElementById("div1");
+
+  for (var i = 0;i < 3; i++){
+    ele.innerHTML += name[i].char;
+    ele.innerHTML += ' big5= ';
+    ele.innerHTML += name[i].big5;
+    ele.innerHTML += ' cns= ';
+    ele.innerHTML += name[i].cns;
+    ele.innerHTML += ' unicode= ';
+    ele.innerHTML += name[i].unicode;
+    ele.innerHTML += ' <br> ';
+    }
 }
 
 /*
@@ -16,3 +48,4 @@ true - The event handler is executed in the capturing phase
 false- Default. The event handler is executed in the bubbling phase
 */
 window.addEventListener( "load", start, false );
+
